4045412d4314fa37ff08c8dad2defd25e3906ab7
Sargis Markosyan <sargismarkosyan5@gmail.com>
2020-12-13 20:46:47 +0400

    3. add player

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     startGame(gameBoard)
 })
 
+const playerSize = 30
+
 function startGame(board) {
-    //@TODO add player
+    const context = board.getContext('2d')
+    const player = new Player(playerSize)
+    player.render(context)
     //@TODO add obsticales
     //@TODO add score counter
+}
+
+function Player(size) {
+    this.playerSize = size
+    this.x = 0
+    this.y = 0
+}
+
+Player.prototype.render = function(context) {
+    context.fillStyle = 'orange';
+    context.fillRect(this.x, this.y, this.playerSize, this.playerSize);
 }
