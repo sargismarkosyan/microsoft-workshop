af039d244acfedf9763b4104043bc9a4968d2d39
Sargis Markosyan <sargismarkosyan5@gmail.com>
2020-12-13 20:14:43 +0400

    2. The game board

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const gameBoard = document.getElementById('game-board')
+    startGame(gameBoard)
+})
+
+function startGame(board) {
+    //@TODO add player
+    //@TODO add obsticales
+    //@TODO add score counter
+}
+
